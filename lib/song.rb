14a38044ba68ed2fbc9015ed09da43dbe95ad737
MARTINA_WEIDENBAUM <martinaweidenbaum1@MARTINA-WEIDENBAUMs-MacBook-Pro.local>
2017-07-26 21:38:41 -0400

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,9 @@
-require 'pry'
-
 class Song
+
+extend Memorable::ClassMethods
+#include Memorable::InstanceMethods
+
+
   attr_accessor :name
   attr_reader :artist
 
@@ -18,17 +21,16 @@ class Song
     @artist = artist
   end
 
-  def self.reset_all
-    @@songs.clear
-  end
+  # def self.reset_all
+  #   @@songs.clear
+  # end
 
   def to_param
     name.downcase.gsub(' ', '-')
   end
 
-  def self.count
-    self.all.count
-  end
+  # def self.count
+  #   self.all.count
 
   def self.all
     @@songs
